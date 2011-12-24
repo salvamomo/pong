b613602a9d49cc213bc6e9018d3d3868e3966b6e
salvamomo <inthorinrain@gmail.com>
2011-12-24 02:57:40 +0100

    Some changes to main class and inputhandler finished for basic keyboard input. In a noobish way, though\!

--- src/com/salvamomo/pong/Game.java
+++ src/com/salvamomo/pong/Game.java
@@ -8,10 +8,12 @@ package com.salvamomo.pong;
  */
 
 import java.awt.Canvas;
+import java.awt.Dimension;
 import java.awt.Graphics2D;
 import java.awt.image.BufferStrategy;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class Game implements Runnable {
    // Variables regarding game execution and fps. Needed to keep the game loop running!
@@ -28,20 +30,54 @@ public class Game implements Runnable {
 	Canvas canvas;
 	BufferStrategy bufferStrategy;
 	
+	InputHandler input = new InputHandler(this);
+	
 	// TODO: Decide whether for this game the players should be two separate variables.
 	Player players[]; // We have at least 2 players!!
+	private boolean matchRunning = false;
 	
 	public Game () {
+		frame = new JFrame("Pong");
+		JPanel panel = (JPanel) frame.getContentPane();
+		panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+
+		panel.setLayout(null);
 		
+		canvas = new Canvas();
+		canvas.setBounds(0, 0, WIDTH, HEIGHT);
+		canvas.setIgnoreRepaint(true);
+		
+		panel.add(canvas);
+		canvas.addKeyListener(input);
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.pack();
+		frame.setResizable(false);
+		frame.setVisible(true);
+		
+		//player = new Player();
+		
+	    canvas.createBufferStrategy(2);
+	    bufferStrategy = canvas.getBufferStrategy();
+	    canvas.requestFocus();
 	}
 
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
-		System.out.println("Ey yo! \\o/");
+		System.out.printf("Ey yo! \\o/.  Match running?: %b", matchRunning);
 		while (running) {
 			
+			
+			if (matchRunning) {
+				System.out.println("Match is on fire! -- Press <enter> to Pause it");
+			}
+			
 		}
+	}
+	
+	public void resume() {
+		this.matchRunning = !matchRunning;
 	}
 	
 	/*
--- src/com/salvamomo/pong/InputHandler.java
+++ src/com/salvamomo/pong/InputHandler.java
@@ -5,6 +5,13 @@ import java.awt.event.KeyListener;
 public class InputHandler implements KeyListener {
 	Game game;
 	
+	// For now, using 4 static variables for up-down moves for 2 players
+	public boolean up;
+	public boolean down;
+	public boolean w;
+	public boolean s;
+	
+	
 	public InputHandler(Game game) {
 		this.game = game;
 	}
@@ -12,19 +19,53 @@ public class InputHandler implements KeyListener {
 	@Override
 	public void keyPressed(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
+		switch (e.getKeyCode()) {
+			// Fires/Pauses the match	
+			case KeyEvent.VK_ENTER:
+				game.resume();
+				break;
+			case KeyEvent.VK_UP:
+				up = true;
+				if (down == true) down = false;
+				break;
+			case KeyEvent.VK_DOWN:
+				down = true;
+				if (up == true) up = false;
+				break;
+			case KeyEvent.VK_W:
+				w = true;
+				if (s == true) s = false;
+				break;
+			case KeyEvent.VK_S:
+				s = true;
+				if (w == true) w = false;
+				break;
+		}
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
+		switch (e.getKeyCode()) {
+		case KeyEvent.VK_UP:
+			up = false;
+			break;
+		case KeyEvent.VK_DOWN:
+			down = false;
+			break;
+		case KeyEvent.VK_W:
+			w = false;
+			break;
+		case KeyEvent.VK_S:
+			s = false;
+			break;
+		}
 	}
 
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
+		// Not used for now!
 	}
 	
 }
